eb470cbf106e37075fb6846d9d4a55aa2bf3adb8
alemiranda7 <7alemiranda@gmail.com>
2022-08-04 17:23:50 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from "react";
 
-function App() {
-  return (
+let alex = {
+  cliente: "Alex",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Celular", preco: "R$ 3000" },
+    { nome: "Geladeira", preco: "R$ 1500" },
+  ],
+  situacao: true,
+};
+
+let joyce = {
+  cliente: "Joyce",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Celular", preco: "R$ 5600" },
+    { nome: "Geladeira", preco: "R$ 8000" },
+  ],
+  situacao: false,
+};
+
+let dados = joyce;
+
+const App = () => {
+  let total = dados.compras
+    .map((item) => +item.preco.replace("R$ ", ""))
+    .reduce((a, b) => a + b);
+
+  return (  
     <>
-    <div>Helo world!</div>
+      <div>
+        <p>Nome: {dados.cliente}</p>
+        <p>Idade: {dados.idade}</p>
+        <p>
+          Situação: {' '}
+          <span style={{ color: dados.situacao ? "green" : "red" }}>
+            {dados.situacao ? "Ativo" : "Inativo"}
+          </span>
+        </p>
+        <p>Total: {total}</p>
+        <p>{total > 10000 && 'Voce ta gastando muito.'}</p>
+      </div>
     </>
   );
-}
+};
 
 export default App;
